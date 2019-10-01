d59bc33dd2022147f6c054c4d7b0a6c58e5f07bb
Samy Cherfaoui <scherfaoui@berkeley.edu>
2019-10-01 02:27:53 -0700

    Update Tileset.java

--- byog/TileEngine/Tileset.java
+++ byog/TileEngine/Tileset.java
@@ -5,15 +5,6 @@ import java.awt.Color;
 /**
  * Contains constant tile objects, to avoid having to remake the same tiles in different parts of
  * the code.
- *
- * You are free to (and encouraged to) create and add your own tiles to this file. This file will
- * be turned in with the rest of your code.
- *
- * Ex:
- *      world[x][y] = Tileset.FLOOR;
- *
- * The style checker may crash when you try to style check this file due to use of unicode
- * characters. This is OK.
  */
 
 public class Tileset {
